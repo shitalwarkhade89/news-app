99d42bc8abc13bd7e40369fba704ae8b94dd9237
shitalwarkhade89 <shitalwarkhade89@gmail.com>
2023-10-16 16:24:37 +0530

    news added

--- src/componets/NewsArticle/NewsArticle.js
+++ src/componets/NewsArticle/NewsArticle.js
@@ -1,2 +1,18 @@
 import React from "react";
 import './NewsArticle.css'
+
+function NewsArticle({author, title, description, url, urlToImage, publishedAt, content}) {
+    return (
+        <>
+            <div className="news-article-card">
+                <img src={urlToImage} alt="" className="news-article-img" />
+                <h1 className="article-title">{title}</h1>
+                <div className="article-info-div">
+                    <p className="article-author">{author}</p>
+                    <p className="article-publishat">{publishedAt}</p>
+                </div>
+            </div>
+        </>
+    )
+}
+export default NewsArticle
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import './Home.css'
-import  axios from "axios"
+import axios from "axios"
+import NewsArticle from "../../componets/NewsArticle/NewsArticle";
 
 function Home() {
     const [news, setNews] = useState([])
@@ -8,7 +9,7 @@ function Home() {
     const loadNews = async () => {
         const response = await axios.get(`https://newsapi.org/v2/everything?q=apple&from=2023-10-15&to=2023-10-15&sortBy=popularity&apiKey=2477dff7c9a54a599439423d03af1ba2`)
 
-            setNews(response.data.articles)
+        setNews(response.data.articles)
     }
     useEffect(() => {
         loadNews()
@@ -16,23 +17,21 @@ function Home() {
     return (
         <>
             <h1>News App</h1>
-            {
-                news.map((newsArticle,index) => {
-                    const {author,title,description,url,urlToImage,publishedAt,content} =newsArticle
-                   return(
-                        <>
-                        <div className="news-article-card">
-                            <img src={urlToImage} alt="" className="news-article-img"/>
-                            <h1>{title}</h1>
+            <div className="news-container">
+                {
+                    news.map((newsArticle, index) => {
+                        const { author, title, description, url, urlToImage, publishedAt, content } = newsArticle
+                        return (
+
+                            <NewsArticle author={author} title={title} description=
+                                {description} url={url} urlToImage=
+                                {urlToImage} publishedAt={publishedAt} content={content} key={index} />
 
 
-                        </div>
-                        
-                        </>
-                   ) 
-                })
-            }
-
+                        )
+                    })
+                }
+            </div>
         </>
     )
 
